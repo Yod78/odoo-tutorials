beedc0b98112a2764f8074b6dbaefc0dfbf15a1c
David Parlapiano <david.parlapiano@gmail.com>
2026-04-08 11:29:13 +0200

    owl

--- awesome_owl/static/src/counter/counter.js
+++ awesome_owl/static/src/counter/counter.js
@@ -2,14 +2,19 @@ import { Component, useState } from "@odoo/owl";
 
 
 export class Counter extends Component {
-   static template = "awesome_owl.counter";
-static props = {};
+    static template = "awesome_owl.counter";
+    static props = {
+        onChange: { type: Function, optional: true },
+    };
 
-   setup() {
+    setup() {
         this.state = useState({ value: 0 });
     }
 
     increment() {
         this.state.value++;
+        if (this.props.onChange) {
+            this.props.onChange(this.state.value);
+        }
     }
 }
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,9 +1,21 @@
-import { Component, useState } from "@odoo/owl";
+import { Component, useState, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
+
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
     static props = {};
     static components = { Counter, Card };
+
+    htmlContent = markup("<b>Je suis en gras</b>");
+    escapedContent = "<b>pas de gras</b>";
+
+    setup() {
+        this.sum = useState({ value: 0 });
+    }
+
+    incrementSum() {
+        this.sum.value++;
+    }
 }
